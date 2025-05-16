acf9ccb9f961bec1bb86e0a8720a0b35faed724a
CMEONE <kabir@ramzan.me>
2025-05-16 02:10:39 -0700

    Add user management test cases

--- program/tests/fatira.ts
+++ program/tests/fatira.ts
@@ -8,103 +8,252 @@ import { Token, TOKEN_PROGRAM_ID, createMint, createAccount, mintTo, createSetAu
 import { PublicKey, SystemProgram, Keypair, LAMPORTS_PER_SOL, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
 
 describe("fatira", () => {
-  const provider = anchor.AnchorProvider.env();
-  anchor.setProvider(provider);
-  const program = anchor.workspace.fatira as Program<Fatira>;
+	const provider = anchor.AnchorProvider.env();
+	anchor.setProvider(provider);
+	const program = anchor.workspace.fatira as Program<Fatira>;
 
-  const payer = provider.wallet as anchor.Wallet;
-  const admin = Keypair.fromSecretKey(Uint8Array.from(JSON.parse(fs.readFileSync(`${os.homedir()}/admnewKmeGHkU1ZM8kKaPfunvV4GPmZUAfQ48zNA6fL.json`))));
-  let groupKey: PublicKey;
-  let groupAccount: anchor.web3.AccountInfo<Buffer>;
+	const payer = provider.wallet as anchor.Wallet;
+	const admin = Keypair.fromSecretKey(Uint8Array.from(JSON.parse(fs.readFileSync(`${os.homedir()}/admnewKmeGHkU1ZM8kKaPfunvV4GPmZUAfQ48zNA6fL.json`))));
+	let groupKey: PublicKey;
+	let groupAccount: anchor.web3.AccountInfo<Buffer>;
 
-  let mint: PublicKey;
-  let payerAccount: PublicKey;
-  let escrowAccount: PublicKey;
-  let fakeEscrowAccount: PublicKey;
-  let groupSigner: Keypair;
-  let group: PublicKey;
+	let mint: PublicKey;
+	let payerAccount: PublicKey;
+	let escrowAccount: PublicKey;
+	let fakeEscrowAccount: PublicKey;
+	let groupSigner: Keypair;
+	let group: PublicKey;
+	let user: Keypair;
 
-  before(async () => {
-    await provider.connection.confirmTransaction(
-      await provider.connection.requestAirdrop(admin.publicKey, LAMPORTS_PER_SOL)
-    )
+	before(async () => {
+		await provider.connection.confirmTransaction(
+			await provider.connection.requestAirdrop(admin.publicKey, LAMPORTS_PER_SOL)
+		)
 
-    groupSigner = Keypair.generate();
-    group = groupSigner.publicKey;
-    mint = await createMint(provider.connection, admin, admin.publicKey, null, 0);
-    let [escrowAuthority, escrowBump] = await PublicKey.findProgramAddress([Buffer.from("authority"), group.toBuffer()], program.programId);
+		user = Keypair.generate();
+		groupSigner = Keypair.generate();
+		group = groupSigner.publicKey;
+		mint = await createMint(provider.connection, admin, admin.publicKey, null, 0);
+		let [escrowAuthority, escrowBump] = await PublicKey.findProgramAddress([Buffer.from("authority"), group.toBuffer()], program.programId);
 
-    payerAccount = await createAccount(provider.connection, payer.payer, mint, payer.publicKey);
+		payerAccount = await createAccount(provider.connection, payer.payer, mint, payer.publicKey);
 
-    let temporaryOwner = Keypair.generate();
-    let fakeTemporaryOwner = Keypair.generate();
-    escrowAccount = await createAccount(provider.connection, payer.payer, mint, temporaryOwner.publicKey);
-    fakeEscrowAccount = await createAccount(provider.connection, payer.payer, mint, fakeTemporaryOwner.publicKey);
-    let transferAuthority = new Transaction().add(createSetAuthorityInstruction(
-      escrowAccount,
-      temporaryOwner.publicKey,
-      AuthorityType.AccountOwner,
-      escrowAuthority,
-      [],
-      TOKEN_PROGRAM_ID
-    ));
-    await sendAndConfirmTransaction(provider.connection, transferAuthority, [payer.payer, temporaryOwner], { commitment: "confirmed" });
+		let temporaryOwner = Keypair.generate();
+		let fakeTemporaryOwner = Keypair.generate();
+		escrowAccount = await createAccount(provider.connection, payer.payer, mint, temporaryOwner.publicKey);
+		fakeEscrowAccount = await createAccount(provider.connection, payer.payer, mint, fakeTemporaryOwner.publicKey);
+		let transferAuthority = new Transaction().add(createSetAuthorityInstruction(
+			escrowAccount,
+			temporaryOwner.publicKey,
+			AuthorityType.AccountOwner,
+			escrowAuthority,
+			[],
+			TOKEN_PROGRAM_ID
+		));
+		await sendAndConfirmTransaction(provider.connection, transferAuthority, [payer.payer, temporaryOwner], { commitment: "confirmed" });
 
-    await mintTo(provider.connection, admin, mint, payerAccount, admin, 1000);
-  });
+		await mintTo(provider.connection, admin, mint, payerAccount, admin, 1000);
+	});
 
-  it("Fails to create a group with invalid escrow", async () => {
-    try {
-      await program.methods.createGroup().accounts({
-        group,
-        payer: payer.publicKey,
-        currency: mint,
-        escrow: fakeEscrowAccount,
-        admin: admin.publicKey,
-        systemProgram: SystemProgram.programId
-      }).signers([admin, groupSigner]).rpc();
+	it("Fails to create a group with invalid escrow", async () => {
+		try {
+			await program.methods.createGroup().accounts({
+				group,
+				payer: payer.publicKey,
+				currency: mint,
+				escrow: fakeEscrowAccount,
+				admin: admin.publicKey,
+				systemProgram: SystemProgram.programId
+			}).signers([admin, groupSigner]).rpc();
 
-      assert.fail();
-    } catch(err) {
-      const anchorError = err as AnchorError;
-      assert.equal(anchorError.error.errorCode.code, "InconsistentEscrowOwner");
-    }
-  });
+			assert.fail();
+		} catch(err: AnchorError) {
+			assert.equal(err.error.errorCode.code, "InconsistentEscrowOwner");
+		}
+	});
 
-  it("Fails to create a group with invalid admin keypair", async () => {
-    let fakeAdmin = Keypair.generate();
+	it("Fails to create a group with invalid admin keypair", async () => {
+		let fakeAdmin = Keypair.generate();
 
-    try {
-      await program.methods.createGroup().accounts({
-        group,
-        payer: payer.publicKey,
-        currency: mint,
-        escrow: escrowAccount,
-        admin: fakeAdmin.publicKey,
-        systemProgram: SystemProgram.programId
-      }).signers([fakeAdmin, groupSigner]).rpc();
+		try {
+			await program.methods.createGroup().accounts({
+				group,
+				payer: payer.publicKey,
+				currency: mint,
+				escrow: escrowAccount,
+				admin: fakeAdmin.publicKey,
+				systemProgram: SystemProgram.programId
+			}).signers([fakeAdmin, groupSigner]).rpc();
 
-      assert.fail();
-    } catch(err) {
-      const anchorError = err as AnchorError;
-      assert.equal(anchorError.error.errorCode.code, "Unauthorized");
-    }
-  });
+			assert.fail();
+		} catch(err: AnchorError) {
+			assert.equal(err.error.errorCode.code, "Unauthorized");
+		}
+	});
 
-  it("Creates a group", async () => {
-    await program.methods.createGroup().accounts({
-      group,
-      payer: payer.publicKey,
-      currency: mint,
-      escrow: escrowAccount,
-      admin: admin.publicKey,
-      systemProgram: SystemProgram.programId
-    }).signers([admin, groupSigner]).rpc();
+	it("Creates a group", async () => {
+		await program.methods.createGroup().accounts({
+			group,
+			payer: payer.publicKey,
+			currency: mint,
+			escrow: escrowAccount,
+			admin: admin.publicKey,
+			systemProgram: SystemProgram.programId
+		}).signers([admin, groupSigner]).rpc();
 
-    let groupData = await program.account.group.fetch(group);
-    assert.ok(groupData.currency.equals(mint));
-    assert.ok(groupData.escrow.equals(escrowAccount));
-    assert.equal(groupData.balances.length, 1);
-    assert.ok(groupData.balances[0].user.equals(payer.publicKey));
-  });
+		let groupData = await program.account.group.fetch(group);
+		assert.ok(groupData.currency.equals(mint));
+		assert.ok(groupData.escrow.equals(escrowAccount));
+		assert.equal(groupData.balances.length, 1);
+		assert.ok(groupData.balances[0].user.equals(payer.publicKey));
+	});
+
+	it("Fails to add a user to a group without authorization", async () => {
+		try {
+			await program.methods.addUser(user.publicKey).accounts({
+				group,
+				payer: user.publicKey
+			}).signers([user]).rpc();
+
+			assert.fail();
+		} catch(err: AnchorError) {
+			assert.equal(err.error.errorCode.code, "UnauthorizedAdd");
+		}
+	});
+
+	it("Adds a user to a group", async () => {
+		await program.methods.addUser(user.publicKey).accounts({
+			group,
+			payer: payer.publicKey
+		}).rpc();
+
+		let groupData = await program.account.group.fetch(group);
+		assert.ok(groupData.balances[1].user.equals(user.publicKey));
+		assert.equal(groupData.balances[1].approved, false);
+	});
+
+	it("Fails to add a user already in a group", async () => {
+		try {
+			await program.methods.addUser(user.publicKey).accounts({
+				group,
+				payer: payer.publicKey
+			}).rpc();
+
+			assert.fail();
+		} catch(err: AnchorError) {
+			assert.equal(err.error.errorCode.code, "UserAlreadyExists");
+		}
+	});
+
+	it("Fails to transfer admin without approval", async () => {
+		try {
+			await program.methods.transferAdmin(user.publicKey).accounts({
+				group,
+				payer: payer.publicKey
+			}).rpc();
+
+			assert.fail();
+		} catch(err: AnchorError) {
+			assert.equal(err.error.errorCode.code, "UserNotApproved");
+		}
+	});
+
+	it("Approves a user in a group", async () => {
+		await program.methods.approveUser().accounts({
+			group,
+			payer: user.publicKey
+		}).signers([user]).rpc();
+
+		let groupData = await program.account.group.fetch(group);
+		assert.equal(groupData.balances[1].approved, true);
+	});
+
+	it("Fails to transfer admin without authorization", async () => {
+		try {
+			await program.methods.transferAdmin(user.publicKey).accounts({
+				group,
+				payer: user.publicKey
+			}).signers([user]).rpc();
+
+			assert.fail();
+		} catch(err: AnchorError) {
+			assert.equal(err.error.errorCode.code, "UnauthorizedTransfer");
+		}
+	});
+
+	it("Transfers admin in a group", async () => {
+		await program.methods.transferAdmin(user.publicKey).accounts({
+			group,
+			payer: payer.publicKey
+		}).rpc();
+
+		let groupData = await program.account.group.fetch(group);
+		assert.ok(groupData.balances[0].user.equals(user.publicKey));
+		assert.ok(groupData.balances[1].user.equals(payer.publicKey));
+
+		await program.methods.transferAdmin(payer.publicKey).accounts({
+			group,
+			payer: user.publicKey
+		}).signers([user]).rpc();
+
+		groupData = await program.account.group.fetch(group);
+		assert.ok(groupData.balances[0].user.equals(payer.publicKey));
+		assert.ok(groupData.balances[1].user.equals(user.publicKey));
+	});
+
+	it("Fails to remove a user in a group without authorization", async () => {
+		try {
+			let dummyKeypair = Keypair.generate();
+			await program.methods.removeUser(user.publicKey).accounts({
+				group,
+				payer: dummyKeypair.publicKey
+			}).signers([dummyKeypair]).rpc();
+
+			assert.fail();
+		} catch(err: AnchorError) {
+			assert.equal(err.error.errorCode.code, "UnauthorizedRemove");
+		}
+	});
+
+	it("Removes a user from a group", async () => {
+		await program.methods.removeUser(user.publicKey).accounts({
+			group,
+			payer: payer.publicKey
+		}).rpc();
+
+		let groupData = await program.account.group.fetch(group);
+		assert.equal(groupData.balances.length, 1);
+	});
+
+	it("Removes a user from a group as user", async () => {
+		await program.methods.addUser(user.publicKey).accounts({
+			group,
+			payer: payer.publicKey
+		}).rpc();
+
+		let groupData = await program.account.group.fetch(group);
+		assert.equal(groupData.balances.length, 2);
+
+		await program.methods.removeUser(user.publicKey).accounts({
+			group,
+			payer: user.publicKey
+		}).signers([user]).rpc();
+
+		groupData = await program.account.group.fetch(group);
+		assert.equal(groupData.balances.length, 1);
+	});
+
+	it("Fails to remove admin from group", async () => {
+		try {
+			await program.methods.removeUser(payer.publicKey).accounts({
+				group,
+				payer: payer.publicKey
+			}).rpc();
+
+			assert.fail();
+		} catch(err: AnchorError) {
+			assert.equal(err.error.errorCode.code, "CannotRemoveAdmin");
+		}
+	});
+
 });
